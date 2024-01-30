0cf9a5d7ab9a5ea34158ab504c0892eeef2ac8d6
Marie <marie@cleppien.net>
2024-01-30 15:03:08 +0100

    Feat: respawn player after entering death zone

---
+++ assets/scripts/Player/movement/PlayerRespawner.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+func _on_death_zone_body_entered(body) -> void:
+	
+	if (body.name == "Player"):
+		var player: Player = owner.get_node_or_null(NodePath(body.name))
+		
+		if player:
+			# Reset Player's transform
+			player.global_position = self.global_position
