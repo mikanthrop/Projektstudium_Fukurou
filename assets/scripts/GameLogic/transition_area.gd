f05abbdb1c0fbf9c52a033fc2ca18165fc67a729
Marie <marie@cleppien.net>
2024-04-29 16:25:35 +0200

    Feat: scene transition node

---
+++ assets/scripts/GameLogic/transition_area.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@export var scene_to_transition_to: PackedScene;
+var scene_tree: SceneTree;
+
+func ready():
+	pass
+
+func _on_body_entered(body):
+	print("I entered.")
+	scene_tree = get_tree()
+	scene_tree.change_scene_to_packed(scene_to_transition_to)
