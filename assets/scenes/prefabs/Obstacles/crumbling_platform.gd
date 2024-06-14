a187d9ed19b94d67404f813dd77489f6dbc0727f
Marie <marie@cleppien.net>
2024-06-14 15:26:17 +0200

    Fix: random falling through crumbling platforms

--- assets/scenes/prefabs/Obstacles/crumbling_platform.gd
+++ assets/scenes/prefabs/Obstacles/crumbling_platform.gd
@@ -1,4 +1,4 @@
-extends AnimatableBody2D
+extends Area2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var tremble_timer: Timer = $TrembleTimer
@@ -9,22 +9,25 @@ func _ready():
 	animation_player.play("idle")
 
 
-func _on_animation_trigger_body_entered(body):
-	if body.name == "Player": 
-		animation_player.play("tremble")
-		print("tremble timer started")
-		tremble_timer.start()
+func _physics_process(delta):
+	var bodies = get_overlapping_bodies()
+	for body in bodies: 
+		if body.name == "Player":
+			set_physics_process(false)
+			animation_player.play("tremble")
+			tremble_timer.start()
+		else: 
+			animation_player.play("idle")
 
 
 func _on_tremble_timer_timeout():
-	print("tremble timer timed out")
 	animation_player.play("fall")
 
 
-func _on_animation_player_animation_finished(fall):
+func _on_animation_player_animation_finished_fall(anim_name):
 	respawn_timer.start()
 
 
 func _on_respawn_timer_timeout():
+	set_physics_process(true)
 	animation_player.play("idle")
-
