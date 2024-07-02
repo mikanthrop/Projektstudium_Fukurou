af5f43284d8a6ec024b57c5bcfce2c670d2d579c
Marie <marie@cleppien.net>
2024-07-02 12:33:22 +0200

    Fix: minor gameplay bugs

--- assets/scenes/screens/DialogueBox.gd
+++ assets/scenes/screens/DialogueBox.gd
@@ -2,18 +2,19 @@ extends Node2D
 @onready var choice_button_scn = preload("res://ChoiceButton.tscn")
 var choice_buttons: Array[Button] = []
 func _ready():
-	add_text("Bat boys, bat boys, what you gonna do, what you gonna do when we come for you")
-	
+	add_text("Bat boys, bat boys, what you gonna do, what you gonna do when we come for you?")
 
-	
+
 func clear_dialogue_box():
 	for choice in choice_buttons:
 		$VBoxContainer.remove_child(choice)
 	choice_buttons = []
-	
+
+
 func add_text(text: String):
 	$VBoxContainer/Label.text = text
-	
+
+
 func add_choice (choice_text: String):
 	var button_obj: ChoiceButton = choice_button_scn.instantiate()
 	button_obj.choice_index = choice_buttons.size()
@@ -21,11 +22,8 @@ func add_choice (choice_text: String):
 	button_obj.text = choice_text
 	button_obj.choice_selected.connect(_on_choice_selected)
 	$VBoxContainer.add_child(button_obj)
-	
+
 
 func _on_choice_selected(choice_index: int):
 	print(choice_index)
 	pass
-
-
-
