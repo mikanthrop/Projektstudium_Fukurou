c3ff8ad5ef48135cd3f7e51aafbb7e3a2f4b1a16
Marie <marie@cleppien.net>
2024-07-03 00:43:54 +0200

    Feat: dialogue system implemented

--- assets/dialogue/DialogueBox.gd
+++ assets/dialogue/DialogueBox.gd
@@ -1,18 +1,19 @@
 extends Node2D
 
-@onready var choice_button_scn = preload("res://ChoiceButton.tscn")
+@onready var choice_button_scn = preload("res://assets/dialogue/ChoiceButton.tscn")
 @onready var display_box: VBoxContainer = $VBoxContainer
 @onready var display_label: Label = $VBoxContainer/Text
+@onready var ez_dialogue: EzDialogue = $"../EzDialogue"
 
 var choice_buttons: Array[Button] = []
 
 
 func _ready():
-	add_text("Bat boys, bat boys, what you gonna do, what you gonna do when we come for you?")
+	display_box.set_visible(false)
 
 
 func clear_dialogue_box():
-	display_box.test = ""
+	display_label.text = ""
 	for choice in choice_buttons:
 		display_box.remove_child(choice)
 	choice_buttons = []
@@ -24,20 +25,14 @@ func add_text(text: String):
 
 func add_choice (choice_text: String):
 	var button_obj: ChoiceButton = choice_button_scn.instantiate()
+	var button_label : Label= button_obj.get_child(0)
 	button_obj.choice_index = choice_buttons.size()
 	choice_buttons.push_back(button_obj)
-	button_obj.text = choice_text
+	button_label.text = choice_text
 	button_obj.choice_selected.connect(_on_choice_selected)
 	display_box.add_child(button_obj)
 
 
 func _on_choice_selected(choice_index: int):
-	print(choice_index)
-	pass
+	ez_dialogue.next(choice_index)
 
-
-func _on_ez_dialogue_dialogue_generated(response: DialogueResponse):
-	add_text(response.text)
-	for choice in response.choices:
-		add_choice(choice)
-	pass # Replace with function body.
---
+++ assets/dialogue/DialogueHandler.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@export var dialogue_json: JSON
+@export var dialogue_style: Theme
+
+@onready var state: Dictionary = {}
+@onready var dialogue_box: Node2D = $DialogueBox
+@onready var ez_dialogue: EzDialogueReader = $EzDialogue
+@onready var dialogue_layout : VBoxContainer = $DialogueBox/VBoxContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	ez_dialogue.start_dialogue(dialogue_json, state)
+	dialogue_layout.theme = dialogue_style
+
+
+func _on_ez_dialogue_dialogue_generated(response: DialogueResponse) -> void:
+	dialogue_box.clear_dialogue_box()
+	
+	dialogue_box.add_text(response.text)
+	for choice in response.choices:
+		dialogue_box.add_choice(choice)
+
+
+func _on_area_2d_body_entered(body):
+	if body is Player: 
+		dialogue_layout.set_visible(true)
+
+
+func _on_area_2d_body_exited(body):
+	if body is Player: 
+		dialogue_layout.set_visible(false)
