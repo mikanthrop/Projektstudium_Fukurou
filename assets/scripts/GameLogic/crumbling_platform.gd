bf8f96cd0c2e7e1cfb93151f9837e396f3fa4390
Marie <marie@cleppien.net>
2024-08-15 15:58:35 +0200

    Fix: falling through crumbling platforms

--- assets/scripts/GameLogic/crumbling_platform.gd
+++ assets/scripts/GameLogic/crumbling_platform.gd
@@ -4,6 +4,9 @@ extends Area2D
 @onready var tremble_timer: Timer = $TrembleTimer
 @onready var respawn_timer: Timer = $RespawnTimer
 @onready var crumbling = $crumbling
+
+# moves the animation to compensate for irregular height during idling stat but constant height during trembling animation
+var animation_offset: float = 5.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animation_player.play("idle")
@@ -12,15 +15,18 @@ func _ready():
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies: 
-		if body.name == "Player":
-			set_physics_process(false)
-			animation_player.play("tremble")
-			tremble_timer.start()
-		else: 
-			animation_player.play("idle")
+		if body is Player:
+			var children: Array[Node] = body.get_children()
+			for child in children: 
+				if child.name == "FeetCollision" and body.is_on_floor:
+					self.set_position(Vector2(self.get_position().x, self.get_position().y + animation_offset)) 
+					set_physics_process(false)
+					animation_player.play("tremble")
+					tremble_timer.start()
 
 
 func _on_tremble_timer_timeout():
+	self.set_position(Vector2(self.get_position().x, self.get_position().y - animation_offset))
 	crumbling.play()
 	animation_player.play("fall")
 
@@ -30,5 +36,6 @@ func _on_animation_player_animation_finished_fall(anim_name):
 
 
 func _on_respawn_timer_timeout():
+	self.set_position(Vector2(self.get_position().x, self.get_position().y - animation_offset))
 	set_physics_process(true)
 	animation_player.play("idle")
